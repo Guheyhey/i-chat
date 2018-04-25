60e5ced0ad4b1774cf2250780da10a61782135fd
Guheyhey <joeglancith@hotmail.com>
2018-04-24 22:53:21 -0700

    add RecyclerView dependency

---
+++ common/src/main/java/io/guheyhey/ichat/common/widget/recycler/RecyclerAdapter.java
@@ -0,0 +1,12 @@
+package io.guheyhey.ichat.common.widget.recycler;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Guheyhey
+ * Date: 2018/4/24
+ * Time: 21:34
+ * Description:
+ */
+public class RecyclerAdapter {
+}
