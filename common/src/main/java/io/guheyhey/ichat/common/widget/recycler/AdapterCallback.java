cee7a9bad1e3ba78a18c1026717101e225975417
AL <alvin.liao.3000@gmail.com>
2018-04-24 23:00:40 -0700

    Add recycle view base class (#1) Tag 1.0.0
    * add RecyclerView dependency
    
    * Create AdapterCallback class for RecyclerAdapter
    
    * add resource file to store Tag id of ViewHolder
    
    * create RecyclerAdapter base class

---
+++ common/src/main/java/io/guheyhey/ichat/common/widget/recycler/AdapterCallback.java
@@ -0,0 +1,11 @@
+package io.guheyhey.ichat.common.widget.recycler;
+
+/**
+ * @author Guheyhey
+ * Date: 2018/4/24
+ * Time: 21:34
+ * Description:
+ */
+public interface AdapterCallback<Data> {
+    void update(Data data, RecyclerAdapter.ViewHolder<Data> holder);
+}
